f7e7012af8ddeedfc73ebd9c39b3f38df4e9d9af
Bill St. Clair <billstclair@gmail.com>
2016-11-02 20:12:50 -0400

    Put the model back in simulated.elm. Now that the xxxReceiver functions received the database as an arg, the recursive definition in no longer a problem.

--- examples/simulated.elm
+++ examples/simulated.elm
@@ -10,7 +10,6 @@
 ----------------------------------------------------------------------
 
 import DynamoBackend as DB
-import ExampleModel exposing (..)
 
 import Html exposing (Html, div, h1, text, input, button, a, img, p)
 import Html.Attributes exposing (href, id, alt, src, width, height, style)
@@ -31,6 +30,73 @@ main =
     }
 
 -- MODEL
+
+type alias Model =
+  { dbDict : DB.StringDict      -- used by the backend simulator
+  , profile : Maybe DB.Profile  -- Nothing until logged in
+  , keys : List String          -- keys returned by DB.scan
+  , valueDict : DB.StringDict   -- used by this code to cache key/value pairs
+  , key : String                -- displayed key input
+  , value : String              -- displayed value input
+  , error : String
+  }
+  
+profile : DB.Profile
+profile =
+  DB.Profile "someone@somewhere.net" "John Doe" "random-sequence-1234"
+
+loginReceiver : DB.Profile -> DB.Database Model Msg -> Model -> (Model, Cmd Msg)
+loginReceiver profile database model =
+  ( { model | profile = Just profile }
+  , DB.scan 0 database model
+  )
+
+getReceiver : String -> String -> DB.Database Model Msg -> Model -> (Model, Cmd Msg)
+getReceiver key value database model =
+  ( { model |
+      value = value
+    , valueDict = Dict.insert key value model.valueDict
+    }
+  , Cmd.none
+  )
+
+putReceiver : String -> String -> DB.Database Model Msg -> Model -> (Model, Cmd Msg)
+putReceiver key value database model =
+  ( { model | valueDict = Dict.insert key value model.valueDict }
+  , Cmd.none
+  )
+
+scanReceiver : List String -> DB.Database Model Msg -> Model -> (Model, Cmd Msg)
+scanReceiver keys database model =
+  ( { model |
+      keys = keys
+    , valueDict = Dict.empty
+    }
+  , Cmd.none
+  )
+
+logoutReceiver : DB.Database Model Msg -> Model -> (Model, Cmd Msg)
+logoutReceiver database model =
+  ( { model |
+      profile = Nothing
+    , key = ""
+    , value = ""
+    , keys = []
+    , valueDict = Dict.empty
+    }
+  , Cmd.none
+  )
+
+backendCmd : Int -> DB.Properties -> Cmd Msg
+backendCmd tag properties =
+  Task.perform
+    (\x -> Nop)
+    (\x -> BackendMsg tag properties)
+    (Task.succeed 1)
+
+setDbDict : Dict String String -> Model -> Model
+setDbDict dict model =
+  { model | dbDict = dict }
 
 database : DB.Database Model Msg
 database =
@@ -55,6 +121,16 @@ init =
   )
 
 -- UPDATE
+
+type Msg
+  = UpdateKey String
+  | UpdateValue String
+  | Login
+  | Logout
+  | Get
+  | Put
+  | BackendMsg Int DB.Properties
+  | Nop
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
